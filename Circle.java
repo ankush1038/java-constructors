b64b313db7aca219e666e71363f476a9c6788b93
ankush1038 <ankusharma1029@gmail.com>
2025-03-10 22:47:59 +0530

    [Ankush] Add: Parameterised and default comnstructor made

--- Circle.java
+++ Circle.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Circle {
     public static void main(String[] args) {
 
-        java.util.Scanner sc = new java.util.Scanner(System.in);
+        Scanner sc = new java.util.Scanner(System.in);
 
         System.out.println("Enter Radius : ");
         double radius = sc.nextDouble();
